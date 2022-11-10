13b9fc1f7cf6feb6d983925dcb598f5f39ef2bb5
Spike <l-yanlei@hotmail.com>
2022-11-10 19:00:35 +0800

    feat: add function to copy text

--- lisp/my-lisp/init-wsl.el
+++ lisp/my-lisp/init-wsl.el
@@ -11,5 +11,13 @@
           browse-url-generic-args     cmd-args
           browse-url-browser-function 'browse-url-generic)))
 
+(defun spike-leung/wsl-copy ()
+  "Copy region to windows clipboard."
+  (interactive)
+  (progn
+    (write-region (mark) (point) "~/copy.txt" nil nil nil nil)
+    (shell-command "cat ~/copy.txt | clip.exe")
+    ))
+
 (provide 'init-wsl)
 ;;; init-wsl.el ends here
