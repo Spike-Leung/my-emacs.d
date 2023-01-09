dc34f8c526edac0ee531f3730c7f20bc72bc4e5c
Spike <l-yanlei@hotmail.com>
2023-01-09 19:05:34 +0800

    :sparkles: feat: set transparent background

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -3,6 +3,7 @@
 ;;; Commentary:
 ;;; Code:
 (set-face-attribute 'default nil :height 150 :family "Hack Nerd Font")
-
+;; set transparent background
+(set-face-background 'default "unspecified-bg")
 (provide 'init-face)
 ;;; init-face.el ends here
