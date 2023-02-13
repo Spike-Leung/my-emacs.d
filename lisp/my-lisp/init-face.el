e22d004b05711f1d08112ea116edfbd798d49093
Spike <l-yanlei@hotmail.com>
2023-02-13 09:19:12 +0800

    refactor: disable transparent background

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -4,6 +4,6 @@
 ;;; Code:
 (set-face-attribute 'default nil :height 150 :family "Hack Nerd Font")
 ;; set transparent background
-(set-face-background 'default "unspecified-bg")
+;; (set-face-background 'default "unspecified-bg")
 (provide 'init-face)
 ;;; init-face.el ends here
