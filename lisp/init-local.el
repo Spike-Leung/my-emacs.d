b5fb6308919f0211edb4cb3f80d167a42de5655e
Spike <l-yanlei@hotmail.com>
2023-05-23 01:53:28 +0800

    :recycle: refine my-lisp

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,37 +1,25 @@
-;;; Package --- Summary
-;; init-local
+;;; init-local.el --- load local custom configs
 ;; https://github.com/purcell/emacs.d
-
-
 ;;; Commentary:
 ;;; Code:
 (add-to-list 'load-path (expand-file-name "lisp/my-lisp" user-emacs-directory))
 
-(require 'init-my-theme)
-(require 'init-face)
-(require 'init-yasnippet)
+(require 'init-auto-save)
+(require 'init-beancount)
+(require 'init-corfu-terminal)
+(require 'init-easy-hugo)
+(require 'init-elfeed)
+(require 'init-gitmoji)
+(require 'init-modus-theme)
+(require 'init-mu4e)
+(require 'init-my-markdown)
 (require 'init-my-org)
 (require 'init-org-roam)
 (require 'init-org-roam-ui)
-(require 'init-org-download)
-(require 'init-my-markdown)
-(require 'init-easy-hugo)
-(require 'init-extension-mode)
-(require 'init-auto-save)
-(require 'init-custom-function)
+(require 'init-projectile)
 (require 'init-typescript)
-(require 'init-js)
-;; (require 'init-proxy)
-(require 'init-company)
-(require 'init-reset-keybinding)
 (require 'init-vue)
 (require 'init-wsl)
-(require 'init-eglot)
-(require 'init-elfeed)
-(require 'init-projectile)
-(require 'init-gitmoji)
-;; (require 'init-deno-bridge)
-;; (require 'init-translate)
-(require 'init-corfu-terminal)
+(require 'init-yasnippet)
 (provide 'init-local)
 ;;; init-local.el ends here
