be164db02245f42e96d0963be31e726670d65ee5
Spike <l-yanlei@hotmail.com>
2023-05-25 10:02:50 +0800

    :sparkles: add function to set transparent background in terminal

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -5,5 +5,10 @@
 (set-face-attribute 'default nil :height 150 :family "Hack Nerd Font")
 ;; set transparent background
 (set-face-background 'default "unspecified-bg")
+
+(defun spike-leung/set-background-transparent ()
+  "Set background transparent."
+  (interactive)
+  (set-face-background 'default "unspecified-bg"))
 (provide 'init-face)
 ;;; init-face.el ends here
