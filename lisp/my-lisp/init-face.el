3ccee934939f279201bc3659a45c74e4687cfa01
Spike <l-yanlei@hotmail.com>
2023-06-02 10:28:49 +0800

    :sparkles: set frame-background-mode to `light` for `citre` frame

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -4,6 +4,7 @@
 ;;; Code:
 (set-face-attribute 'default nil :height 150 :family "Hack Nerd Font")
 ;; set transparent background
+(setq frame-background-mode "light")
 (set-face-background 'default "unspecified-bg")
 
 (defun spike-leung/set-background-transparent ()
