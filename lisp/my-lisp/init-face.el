be49a40ef37f8e5d645b95fbc4a7db253cbdc2b6
Spike <l-yanlei@hotmail.com>
2023-09-13 14:46:41 +0800

    :sparkles: add toggle-wsl-theme method

--- lisp/my-lisp/init-face.el
+++ lisp/my-lisp/init-face.el
@@ -7,9 +7,17 @@
 (setq frame-background-mode "light")
 (set-face-background 'default "unspecified-bg")
 
+(defun spike-leung/toggle-wsl-theme ()
+  "Toggle dark theme and set background transparent."
+  (interactive)
+  (modus-themes-select 'modus-vivendi-tritanopia)
+  (spike-leung/set-background-transparent))
+
+
 (defun spike-leung/set-background-transparent ()
   "Set background transparent."
   (interactive)
   (set-face-background 'default "unspecified-bg"))
+
 (provide 'init-face)
 ;;; init-face.el ends here
