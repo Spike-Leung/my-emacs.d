26b01eb786c2782049bc2819e4166efe99bb8a31
Spike <l-yanlei@hotmail.com>
2024-02-01 13:46:25 +0800

    refactor: update default theme

--- lisp/my-lisp/init-modus-theme.el
+++ lisp/my-lisp/init-modus-theme.el
@@ -33,8 +33,8 @@
         (bg-diff-context    unspecified)))
 
 ;; Load the theme of your choice:
-(load-theme 'modus-vivendi t t)
-(enable-theme 'modus-vivendi)
+(load-theme 'modus-vivendi-tinted t t)
+(enable-theme 'modus-vivendi-tinted)
 
 (provide 'init-modus-theme)
 ;;; init-modus-theme.el ends here
