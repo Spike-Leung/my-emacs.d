4658b8bc1af97dfbe6c6467a08482d26f4617044
Spike <l-yanlei@hotmail.com>
2024-05-09 09:56:48 +0800

    feat: :sparkles: add  open-jira-issue function

--- lisp/my-lisp/init-my-misc.el
+++ lisp/my-lisp/init-my-misc.el
@@ -7,5 +7,12 @@
           ("~/git" . 1)
           )))
 
+(defun spike-leung/open-jira-issue (issue-id)
+  "Open the JIRA issue with ISSUE-ID in a web browser."
+  (interactive "sEnter JIRA issue ID (e.g., DD-1234): ")
+  (let* ((jira-base-url "https://jira.gyenno.com")
+         (full-url (concat jira-base-url "/browse/" issue-id)))
+    (browse-url full-url)))
+
 (provide 'init-my-misc)
 ;;; init-my-misc.el ends here
