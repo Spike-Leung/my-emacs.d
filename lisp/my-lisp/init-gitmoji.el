754c843316894f4d510eaa94830bddb81d54bf6c
Spike <l-yanlei@hotmail.com>
2024-05-27 09:53:50 +0800

    :recycle: disable gitmoji-commit-mode default

--- lisp/my-lisp/init-gitmoji.el
+++ lisp/my-lisp/init-gitmoji.el
@@ -7,7 +7,8 @@
 (with-eval-after-load 'gitmoji
   (setq gitmoji-selection-backend (list 'consult))
   (setq gitmoji--display-utf8-emoji t)
-  (gitmoji-commit-mode))
+  ;; (gitmoji-commit-mode)
+  )
 
 (provide 'init-gitmoji)
 ;;; init-gitmoji.el ends here
